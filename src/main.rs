de6f09ecf3ea5231a38ee475ca0abda2fa9ddaf3
Clayton Surgeon <claytonsurgeon@gmail.com>
2021-12-29 12:21:22 -0800

    watches a source file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// use std::env;
-// use std::fs;
+use std::env;
+use std::fs;
+use std::path;
 
 // fn main() {
 // 	let filepath = "./example/test.reil";
@@ -11,37 +12,78 @@
 // 	println!("With text:\n{}", contents);
 // }
 
-extern crate notify;
+// extern crate notify;
 
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
-use std::time::Duration;
 
-fn watch() -> notify::Result<()> {
+fn main() {
+	let args: Vec<String> = env::args().skip(1).collect();
+
+	if args.len() < 1 {
+		eprintln!("Usage: reil.exe <source>");
+		std::process::exit(1);
+	}
+
+	read_file(&args[0]); // first run
+
 	// Create a channel to receive the events.
 	let (tx, rx) = channel();
 
-	// Automatically select the best implementation for your platform.
-	// You can also access each implementation directly e.g. INotifyWatcher.
-	let mut watcher: RecommendedWatcher =
-		Watcher::new(tx, Duration::from_secs(2))?;
+	// Create a watcher object, delivering raw events.
+	// The notification back-end is selected based on the platform.
+	let mut watcher = raw_watcher(tx).unwrap();
 
 	// Add a path to be watched. All files and directories at that path and
 	// below will be monitored for changes.
-	watcher.watch("./example/test.reil", RecursiveMode::Recursive)?;
+	watcher.watch(&args[0], RecursiveMode::Recursive).unwrap();
 
-	// This is a simple loop, but you may want to use more complex logic here,
-	// for example to handle I/O.
 	loop {
 		match rx.recv() {
-			Ok(event) => println!("{:?}", event),
+			Ok(RawEvent {
+				path: Some(path),
+				op: Ok(op),
+				cookie,
+			}) => {
+				println!("{:?} {:?} ({:?})", op, path, cookie);
+				event_router(
+					op,
+					&path.into_os_string().into_string().unwrap(),
+				);
+			}
+			Ok(event) => println!("broken event: {:?}", event),
 			Err(e) => println!("watch error: {:?}", e),
 		}
 	}
 }
 
-fn main() {
-	if let Err(e) = watch() {
-		println!("error: {:?}", e)
-	}
+fn event_router(operation: notify::Op, path: &String) {
+	match operation {
+		notify::op::WRITE => {
+			read_file(path);
+		}
+		_ => {}
+	};
+}
+
+fn read_file(path: &String) {
+	let entryfile = match fs::read_to_string(path) {
+		Ok(v) => v,
+		Err(e) => {
+			eprintln!(
+				"Error: failed to read from the file '{}': {:?}",
+				path, e
+			);
+			std::process::exit(1);
+		}
+	};
+
+	// dbg!(entryfile);
+	tokenizer(&entryfile);
+	// dbg!(entryfile);
+	// unimplemented!();
+}
+
+fn tokenizer(input: &String) {
+	println!("{}", input);
 }
